36449eaca8bfe13837b77ecffd6d248fbfb83b93
faizanrahman <faizanrahman89@gmail.com>
2019-04-25 18:54:34 -0500

    Added BST.js

---
+++ BinarySearchTrees/bst.js
@@ -0,0 +1,17 @@
+// A tree represents a parent/child node relationship. A node can point to another node below them(child),
+// but not on the same level.
+// In a linked list, a node only points to one node, whereas in a tree, a node can point to multiple nodes.
+// Also, there is only 1 entry point(root) in a tree.
+
+// Uses for trees
+// HTML DOM - Elements inside elements inside elements
+// Network Routing
+// Abstract Syntax Trees - Used for syntax of a programming language.
+// Artificial Intelligence - Decision Tree(tic tac toe game as an example)
+// Folders in OS
+// Computer File Systems
+// JSON object returned from API
+
+// Anything that is hierarchial
+
+
